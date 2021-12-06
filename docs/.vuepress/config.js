fc1caeb9e0bed38b83aab12c40e637367551ec7b
YuDGang <liang1304568590@163.com>
2021-12-07 01:01:15 +0800

    add: first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'lil_Who_log',
+  description: 'Write the Code, change the world',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ],
+    
+    sidebar: [
+      '/',
+      // ['/components', '业务组件'],
+    ],
+    // logo: '/assets/img/logo.png',
+  }
+}
